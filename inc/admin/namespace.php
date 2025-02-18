9f4b72246498b81a6782b8b5a284f90d4fdca576
Ryan McCue <me@ryanmccue.info>
2025-02-18 17:06:02 +0000

    Display quota and warning

--- inc/admin/namespace.php
+++ inc/admin/namespace.php
@@ -14,12 +14,12 @@ const MENU_SLUG = 'smartcache';
  */
 function bootstrap() : void {
 	add_action( 'admin_menu', __NAMESPACE__ . '\\register_admin_page' );
+	add_action( 'admin_init', __NAMESPACE__ . '\\register_settings' );
 	add_action( 'admin_init', __NAMESPACE__ . '\\check_on_invalidate_urls_submit' );
 
 	require_once ABSPATH . '/wp-admin/includes/class-wp-list-table.php';
 	require_once __DIR__ . '/class-log-list-table.php';
 }
-
 
 function register_admin_page() : void {
 	add_submenu_page(
@@ -30,6 +30,54 @@ function register_admin_page() : void {
 		MENU_SLUG,
 		__NAMESPACE__ . '\\render_settings_page'
 	);
+}
+
+function register_settings() {
+	add_settings_section(
+		'smartcache-quota',
+		__( 'Quota', 'smartcache' ),
+		__NAMESPACE__ . '\\render_quota_section',
+		'smartcache'
+	);
+}
+
+function render_quota_section() {
+	$usage = Smartcache\get_invalidation_quota_usage();
+	$quota = Smartcache\get_invalidation_quota();
+
+	$is_warning = $usage >= ( 0.8 * $quota );
+	$is_full = $usage >= $quota;
+	$class = $is_warning ? 'warning' : ( $is_full ? 'error' : '' );
+	?>
+		<table class="form-table">
+			<tr>
+				<th scope="row">
+					Monthly quota usage
+				</th>
+				<td>
+					<meter
+						class="usage-meter <?php echo sanitize_html_class( $class ); ?>"
+						high="<?php echo esc_attr( 0.8 * $quota ); ?>"
+						max="<?php echo esc_attr( $quota ); ?>"
+						value="<?php echo esc_attr( $usage ); ?>"
+						style="width: 20em;"
+					>
+						<?php printf( '%d / %d', $usage, $quota ); ?>
+					</meter>
+
+					<p>
+						<?php
+						printf(
+							__( 'You have used %d of %d invalidation requests this month.', 'smartcache' ),
+							$usage,
+							$quota
+						);
+						?>
+					</p>
+				</td>
+			</tr>
+		</table>
+	<?php
 }
 
 function render_settings_page() : void {
